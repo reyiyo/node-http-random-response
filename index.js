8c49df48537a5717a9237cbab808cf41fb9a78d8
Sergio Orbe <reyiyo@gmail.com>
2017-10-26 00:23:13 -0300

    Implement server

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = process.env.RANDOM_HTTP_SERVER_PORT || 80;
+
+const statusCodes = [200, 201, 301, 304, 400, 401, 403, 404, 500, 502, 503];
+
+const getRandomStatusCode = () => {
+    return statusCodes[Math.floor(Math.random() * statusCodes.length)];
+};
+
+const requestHandler = (request, response) => {
+    response.statusCode = getRandomStatusCode();
+    response.end();
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, err => {
+    if (err) {
+        return console.error('Something is wrong :(', err);
+    }
+
+    console.log(`Server is listening on ${port}`);
+});
