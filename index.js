a91c9d17f88cbaa7ee70b8fc9b509141e53e1945
Sergio Orbe <reyiyo@gmail.com>
2017-10-28 13:55:43 -0300

    add status codes env var

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const port = process.env.RANDOM_HTTP_SERVER_PORT || 80;
 
-const statusCodes = [200, 201, 301, 304, 400, 401, 403, 404, 500, 502, 503];
+let statusCodes;
+
+if (typeof process.env.STATUS_CODES !== 'undefined' && process.env.STATUS_CODES !== null) {
+    statusCodes = JSON.parse(process.env.STATUS_CODES);
+} else {
+    statusCodes = [200, 201, 301, 304, 400, 401, 403, 404, 500, 502, 503];
+}
 
 const getRandomStatusCode = () => {
     return statusCodes[Math.floor(Math.random() * statusCodes.length)];
